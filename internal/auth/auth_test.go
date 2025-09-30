e1bf439a8afa0ad4e210b762d228ba0bb05af6d0
Madhur Sahu <madhursahu@protonmail.com>
2025-09-30 22:00:31 +0530

    [A] Test cases [M] Test workflow to run tests (duh)

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headers := http.Header{}
+	_, err := GetAPIKey(headers)
+
+	if !errors.Is(err, ErrNoAuthHeaderIncluded) {
+		t.Errorf("expected error %v, got %v", ErrNoAuthHeaderIncluded, err)
+	}
+
+	headers.Set("Authorization", "Api 1234")
+	_, err = GetAPIKey(headers)
+
+	if !errors.Is(err, MalformedAuthHeader) {
+		t.Errorf("expected error %v, got %v", MalformedAuthHeader, err)
+	}
+
+	headers.Set("Authorization", "ApiKey 1234")
+	key, err := GetAPIKey(headers)
+
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+
+	if key != "1234" {
+		t.Errorf("expected key 1234, got %v", key)
+	}
+}
